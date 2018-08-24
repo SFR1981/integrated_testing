1372de965fb23067dd2a3c67ae5ebff6d31ecf6e
Stephen Rooney <s.f.rooney@gmail.com>
2018-08-24 10:40:29 +0100

    initial commit

---
+++ tests/unit/calculator_spec.js
@@ -0,0 +1,27 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  })
+
+  it('it should add 1 to 4 and get 5' function(){
+    assert.equal(5, calculator.add(1,4))
+  })
+  //
+  //
+  // Unit Tests:
+  //
+  // - `calculator.add()` - add 1 to 4 and get 5
+  // - `calculator.subtract()` subtract 4 from 7 and get 3
+  // - `calculator.multiply()` - multiply 3 by 5 and get 15
+  // - `calculator.divide()` - divide 21 by 7 and get 3
+
+
+});
