1fe6022efb80efe14c93f260e08c40e636509b62
Stephen Rooney <s.f.rooney@gmail.com>
2018-08-24 12:05:04 +0100

    returning NaN when divide by zero is performed

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -78,6 +78,15 @@ describe('calculator functionality', function() {
       expect(running_total.getAttribute('value')).to.eventually.equal('0.6666666666666666')
     })
 
+    it('it should give expected output for a range of numbers__decimals', function(){
+      running_total = element(by.css('#running_total'))
+      element(by.css('#number2')).click();
+      element(by.css('#operator_divide')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('NaN')
+    })
+
 
 
 
@@ -88,5 +97,5 @@ describe('calculator functionality', function() {
 // You should write tests to ensure the following requirements are met:
 //
 
-// Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+
 // If a number is divided by zero, is the output 'Not a number'? (You will need to modify the Calculator model to meet this requirement).
